59b7775fb3bee063b02973fb26c080bc167c0095
Axel Jorgensen <code@axeljorgensen.info>
2021-07-21 13:48:36 -0700

    introduced final totals

--- base/index.js
+++ base/index.js
@@ -2,7 +2,8 @@ const raidtable = document.getElementById('raidtable');
 
 const tables = document.getElementsByTagName('table');
 const tablesArray = Array.prototype.slice.call(tables);
-tablesArray.shift() 
+tablesArray.shift();
+tablesArray.pop();
 
 for (const table of tablesArray) {
     const cells = table.getElementsByTagName('td');
@@ -30,6 +31,47 @@ for (const table of tablesArray) {
     }
     cells[56].innerText = total;
 }
+
+const dayOneTotal = document.getElementsByClassName('dayone');
+const dayTwoTotal = document.getElementsByClassName('daytwo');
+const dayThreeTotal = document.getElementsByClassName('daythree');
+const dayFourTotal = document.getElementsByClassName('dayfour');
+const dayFiveTotal = document.getElementsByClassName('dayfive');
+const daySixTotal = document.getElementsByClassName('daysix');
+const daySevenTotal = document.getElementsByClassName('dayseven');
+const dayEightTotal = document.getElementsByClassName('dayeight');
+const dayNineTotal = document.getElementsByClassName('daynine');
+const dayTenTotal = document.getElementsByClassName('dayten');
+const dayElevenTotal = document.getElementsByClassName('dayeleven');
+const dayTwelveTotal = document.getElementsByClassName('daytwelve');
+const dayThirteenTotal = document.getElementsByClassName('daythirteen');
+const dayFourteenTotal = document.getElementsByClassName('dayfourteen');
+const dailytotals = [dayOneTotal, dayTwoTotal, dayThreeTotal, dayFourTotal, dayFiveTotal,
+    daySixTotal, daySevenTotal, dayEightTotal, dayNineTotal, dayTenTotal, dayElevenTotal,
+    dayTwelveTotal, dayThirteenTotal, dayFourteenTotal]
+
+for (const day of dailytotals) {
+    let counter = 0;
+    for (let i = 0; i < (day.length - 1); i++) {
+        let personalDailyTotal = parseInt(day[i].innerText);
+        if (isNaN(personalDailyTotal)) {
+            personalDailyTotal = 0;
+        }
+        counter += personalDailyTotal;
+    }
+    day[30].innerText = counter;
+}
+
+const allTotals = document.getElementsByClassName('guilddaily');
+let absoluteTotal = 0;
+for (let totall of allTotals) {
+    const guildDailyTotal = parseInt(totall.innerText);
+    if (isNaN(guildDailyTotal)) {
+        guildDailyTotal = 0;
+    }
+    absoluteTotal += guildDailyTotal;
+}
+document.getElementById('absolutetotal').innerText = absoluteTotal;
 
 const allcells = document.getElementsByTagName('td');
 for (const cell of allcells) {
